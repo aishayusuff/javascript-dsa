33a75c48598c692c971092437e7adfb6db618b33
aishayusuff <aishatyusuffe@gmail.com>
2024-01-24 23:08:33 +0100

    solves maxchar problem

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  //Creat a character map fron the str
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
 
+  //For loop to check number of char occurrence in str
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  //Return char key whose value is the max
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+        max = charMap[char];
+        maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+maxChar("sings");
 module.exports = maxChar;
