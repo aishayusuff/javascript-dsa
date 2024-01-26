e3406d7df1cbf64ee6cdf040ba576b300cca5b2b
aishayusuff <aishatyusuffe@gmail.com>
2024-01-26 02:35:43 +0100

    solves chunk question soln1

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    //create an array to store chunked subarrays
+    const chunkedArr = [];
+
+    for (let element of array) {
+        const lastSubArr = chunkedArr[chunkedArr.length - 1];
+
+        if (!lastSubArr || lastSubArr.length === size) {
+            chunkedArr.push([element]);
+        } else {
+            lastSubArr.push(element);
+        }
+    }
+
+    return chunkedArr;
+
+    //loop through the given array
+
+    //get the last subarray in the new chunked array
+
+    // if chunked array is empty || if last subarray.length === size param
+
+        // create a new subarray containing current element
+
+    // else, add current element to last subarray
+
+    //return chunked array  
+
+
+}
 
 module.exports = chunk;
