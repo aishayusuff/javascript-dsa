3d09aba03ff631190462b08876408a9111f3fe06
aishayusuff <aishatyusuffe@gmail.com>
2024-01-27 22:15:17 +0100

    adds soln2 for finding vowels

--- vowels/index.js
+++ vowels/index.js
@@ -7,23 +7,31 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+//SOLN 1
+// function vowels(str) {
+//     //create a counter variable
+//     //loop through str
+//         // if ( object keys includes [a, e, i, o, u])
+//             //return counter++
+
+//     //return counter
+
+//     let counter = 0;
+
+//     for (let char of str.toLowerCase()) {
+//         if (["a", "e", "i", "o", "u"].includes(char)) {
+//             counter++;
+//         }
+//     }
+//     return counter;
+// }
+
 function vowels(str) {
-    //create a counter variable
-    //loop through str
-        // if ( object keys includes [a, e, i, o, u])
-            //return counter++
+  //use regEEx--> 'g' means [makes sure we don't stop at the first match found]
+  //and 'i' means [case insensitive, which removes use of lowerCase]
+  const matches = str.match(/[aeiou]/gi);
 
-    //return counter
-
-    let counter = 0;
-
-    for (let char of str.toLowerCase()) {
-        if (["a", "e", "i", "o", "u"].includes(char)) {
-            counter++;
-        }
-    }
-
-    return counter;
+  return !matches ? 0 : matches.length;
 }
 
 module.exports = vowels;
