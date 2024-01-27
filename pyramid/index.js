2f939dfa8af4f397c688e169aa141aab118ac8ab
aishayusuff <aishatyusuffe@gmail.com>
2024-01-27 21:52:28 +0100

    adds soln1 for printing pyramid

--- pyramid/index.js
+++ pyramid/index.js
@@ -14,6 +14,22 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const colMidpoint = Math.floor((2 * n - 1) / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = "";
+
+    for (let col = 0; col < 2 * n - 1; col++) {
+      if (colMidpoint - row <= col && colMidpoint + row >= col) {
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
