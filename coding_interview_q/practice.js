c6bfd7f6e17231da2be42d679015e59fbec425f1
aishayusuff <aishatyusuffe@gmail.com>
2024-01-31 22:52:46 +0100

    adds another soln that covers an edge case

--- coding_interview_q/practice.js
+++ coding_interview_q/practice.js
@@ -1,7 +1,17 @@
 //return smaller string
 
 const getsSmaller = function(str1, str2) {
-    return str1.length < str2.length ? str1 : str2
+    const getsSmaller = function(str1, str2) {
+        if (str2.length < str1.length) {
+            return str2
+        } else if (str1.length < str2.length) {
+            return str1
+        } else {
+            return [str1, str2]
+        }
+    } 
+    
+    getsSmaller("dogs", "cats")
 } 
 
 getsSmaller("dogs", "cats")
