66dcab8b1fa68d941bcfb4aef780c8a2f0689cf6
aishayusuff <aishatyusuffe@gmail.com>
2024-02-05 21:59:58 +0100

    adds removeFirst mthd for linkedList

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -59,6 +59,14 @@ class LinkedList {
     clear() {
         this.head = null;
     }
+
+    removeFirst() {
+        if(!this.head) {
+            return;
+        }
+
+        this.head = this.head.next
+    }
 }
 
 
